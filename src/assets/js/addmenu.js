6d2e277d1cb77b0d96e8525218d5417fe13a4fb4
anda1234 <gaitanalexandra@yahoo.com>
2016-07-14 00:33:03 -0700

    Update project

--- src/assets/js/addmenu.js
+++ src/assets/js/addmenu.js
@@ -1,44 +1,52 @@
-// var add = (function(){
-// 	$("title_error_message").hide();
-// 	var error_title=false;
+var add = (function(){
+	$("title_error_message").hide();
+	var error_title=false;
 
-// 	$("#form_title").focusout(function(){
-// 		check_title();
+	$("#form_title").focusout(function(){
+		check_title();
+	});
+
+	$("#felul1").focusout(function(){
+		check_title();
+	});
+
+function check_title()
+{
+	var title_length=$("#form_title").val().length;
+	if(title_length<5 || title_length>50){
+		$("#title_error_message").html("Should between 5-50 characters.");
+		$("#title_error_message").show();
+		error_title=true;
+	}
+	else {
+		$("#title_error_message").hide();
+	}
+	}
+});
+add();
+
+
+
+
+
+
+
+
+// var test = (function(){
+// 	$input = $('#form_title');
+
+// 	$input.focusout(function(){
+// 		if($(this).val().length < 5 || $(this).val().length >=30 ) {
+// 			$('.necessary').html("Campul trebuie sa contina intre 5 - 30 caractere.");
+// 		//	alert("asd");
+// 		}
+// 		else
+// 		{
+// 			$('.necessary').html("");
+
+// 		}
+// 	//	console.log($(this).val().length);
 // 	});
-
-// function check_title()
-// {
-// 	var title_length=$("#form_title").val().length;
-// 	if(title_length<5 || title_length>50){
-// 		$("#title_error_message").html("Should between 5-50 characters.");
-// 		$("#title_error_message").show();
-// 		error_title=true;
-// 	}
-// 	else {
-// 		$("title_error_message").hide();
-// 	}
-// 	}
-
-
 // });
 
-// add();
-
-var test = (function(){
-	$input = $('.form__input');
-
-	$input.focusout(function(){
-		if($(this).val().length < 5 || $(this).val().length >=10 ) {
-			$('.error_title').html("NU-i bun");
-			alert("asd");
-		}
-		else
-		{
-			$('.error_title').html("E bun");
-
-		}
-		console.log($(this).val().length);
-	});
-});
-
-test();
+// test();
